8db3c23689bdc9f2728747d169cf62f9eb852319
Tiffany Zhang <tz.shadowfiles@gmail.com>
2014-01-20 20:43:07 -0500

    Restructured the app.

--- index.php
+++ index.php
@@ -1,30 +1,14 @@
-<!doctype html>
+<?php 
+$page = $_GET['p'];
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>Explorer</title>
-  <meta name="description" content="Explorer">
-  <meta name="author" content="Tiff Zhang">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script src="resources/js.js"></script>
-  <link rel="stylesheet" href="resources/styles.css">
+if(!$page) {
+  $page = 'index';
+}
 
 
-  <!--[if lt IE 9]>
-  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-</head>
-
-<body>
-    <div id="container">
-      <div id="back"></div>
-      <div id="current">
-        You must have javascript enabled to play this game
-      </div>
-      <div id="forward"></div>
-    </div>
-</body>
-</html>
+if(file_exists('templates/' . $page . '.php')) {
+  include('templates/' . $page . '.php');
+} else {
+  include('templates/404.php');
+}
+?>
